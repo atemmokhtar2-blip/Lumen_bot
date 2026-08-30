9dc60ea2a2147785d92e5f0b96ce55ff8be7af3d
atemmokhtar2-blip <atemmokhtar2@gmail.com>
2026-08-30 23:38:49 +0300

    architecture: application mutations for tenants/jobs (lumen/application/handlers/__init__.py)

--- lumen/application/handlers/__init__.py
+++ lumen/application/handlers/__init__.py
@@ -2,16 +2,26 @@ from lumen.application.handlers.tenant_handlers import (
     handle_create_tenant,
     handle_authenticate_tenant,
     handle_get_tenant,
+    handle_update_white_label,
+    handle_rotate_api_key,
 )
 from lumen.application.handlers.job_handlers import (
     handle_create_job,
     handle_get_job,
+    handle_cancel_job,
+    handle_pause_job,
+    handle_resume_job,
 )
 
 __all__ = [
     "handle_create_tenant",
     "handle_authenticate_tenant",
     "handle_get_tenant",
+    "handle_update_white_label",
+    "handle_rotate_api_key",
     "handle_create_job",
     "handle_get_job",
+    "handle_cancel_job",
+    "handle_pause_job",
+    "handle_resume_job",
 ]
