be217f554968d72d1ca0c3c4843b6ed84c4994ef
atemmokhtar2-blip <atemmokhtar2@gmail.com>
2026-08-30 23:46:43 +0300

    architecture(phase1): Balance entity + billing gateway use-cases (lumen/domain/entities/__init__.py)

--- lumen/domain/entities/__init__.py
+++ lumen/domain/entities/__init__.py
@@ -1,5 +1,6 @@
 from lumen.domain.entities.tenant import Tenant
 from lumen.domain.entities.job import Job
 from lumen.domain.entities.invoice import Invoice
+from lumen.domain.entities.balance import Balance
 
-__all__ = ["Tenant", "Job", "Invoice"]
+__all__ = ["Tenant", "Job", "Invoice", "Balance"]
