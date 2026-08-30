cc30412d7eefc852544b627899fbff2cd2fa0d69
atemmokhtar2-blip <atemmokhtar2@gmail.com>
2026-08-30 23:38:44 +0300

    architecture: application mutations for tenants/jobs (lumen/application/commands/__init__.py)

--- lumen/application/commands/__init__.py
+++ lumen/application/commands/__init__.py
@@ -1,4 +1,17 @@
 from lumen.application.commands.create_tenant import CreateTenantCommand
 from lumen.application.commands.create_job import CreateJobCommand
+from lumen.application.commands.update_white_label import UpdateWhiteLabelCommand
+from lumen.application.commands.rotate_api_key import RotateApiKeyCommand
+from lumen.application.commands.cancel_job import CancelJobCommand
+from lumen.application.commands.pause_job import PauseJobCommand
+from lumen.application.commands.resume_job import ResumeJobCommand
 
-__all__ = ["CreateTenantCommand", "CreateJobCommand"]
+__all__ = [
+    "CreateTenantCommand",
+    "CreateJobCommand",
+    "UpdateWhiteLabelCommand",
+    "RotateApiKeyCommand",
+    "CancelJobCommand",
+    "PauseJobCommand",
+    "ResumeJobCommand",
+]
